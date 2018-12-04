49521f9839cb44ab43b0c676dea419d64ff3977f
Burdis <andy.burdis@yahoo.com>
2018-12-04 14:39:02 +0000

    Updated gatling plugin version, reduced load size in scenario

--- src/test/scala/Scenarios.scala
+++ src/test/scala/Scenarios.scala
@@ -6,9 +6,9 @@ class Scenarios extends Simulation with PerfSteps with InjectionSteps {
   val testScenario = scenario("Scenario1").feed(feeder)
     .exec(navToGoogle)
       setUp(testScenario.inject(rampUsers(
-          peakUsers = 10,
-          rampUpDuration = 200,
-          constantDuration = 200,
-          rampDownDuration = 200)
+          peakUsers = 1,
+          rampUpDuration = 10,
+          constantDuration = 10,
+          rampDownDuration = 10)
       )).protocols()
 }
